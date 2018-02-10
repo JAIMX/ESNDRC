5bd1c0b021ce2c8728ef77586c18b1c2870051f2
JAIMX <1052644923@qq.com>
2018-02-10 17:40:46 +0800

    add cuts

---
+++ src/cg/master/cuts/StrongInequality.java
@@ -0,0 +1,31 @@
+package cg.master.cuts;
+
+import org.jorlib.frameworks.columnGeneration.master.cutGeneration.AbstractCutGenerator;
+import org.jorlib.frameworks.columnGeneration.master.cutGeneration.AbstractInequality;
+
+public final class StrongInequality extends AbstractInequality {
+
+	public final int edgeIndex;
+	public final int commodity;
+	
+	public StrongInequality(AbstractCutGenerator maintainingGenerator, int edgeIndex,int commodity) {
+		super(maintainingGenerator);
+		this.edgeIndex=edgeIndex;
+		this.commodity=commodity;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(this==o)
+			return true;
+		else if(!(o instanceof StrongInequality))
+			return false;
+		StrongInequality other=(StrongInequality)o;
+		return (this.edgeIndex==other.edgeIndex&&this.commodity==other.commodity);
+	}
+	
+	@Override
+	public int hashCode() {
+		return this.edgeIndex*19+this.commodity;
+	}
+}
---
+++ src/cg/master/cuts/StrongInequalityGenerator.java
@@ -0,0 +1,13 @@
+package cg.master.cuts;
+
+import org.jorlib.frameworks.columnGeneration.master.cutGeneration.AbstractCutGenerator;
+
+import cg.master.SNDRCMasterData;
+import model.SNDRC;
+
+public final class StrongInequalityGenerator extends AbstractCutGenerator<SNDRC, SNDRCMasterData>{
+
+	public StrongInequalityGenerator(SNDRC modelData) {
+		super()
+	}
+}
