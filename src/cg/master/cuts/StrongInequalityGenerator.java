fd78eef6553b0564c4b92149c758e1e22c689a70
JAIMX <1052644923@qq.com>
2018-02-11 12:36:14 +0800

    finish add cut, ready to modify addcolumns and initialize pricing problems in master class

--- src/cg/master/cuts/StrongInequalityGenerator.java
+++ src/cg/master/cuts/StrongInequalityGenerator.java
@@ -1,13 +1,18 @@
 package cg.master.cuts;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import org.jorlib.frameworks.columnGeneration.master.cutGeneration.AbstractCutGenerator;
 import org.jorlib.frameworks.columnGeneration.master.cutGeneration.AbstractInequality;
 
+import cg.Cycle;
 import cg.master.SNDRCMasterData;
 import ilog.concert.IloException;
+import ilog.concert.IloLinearNumExpr;
+import ilog.concert.IloNumVar;
+import ilog.concert.IloRange;
 import ilog.cplex.IloCplex.UnknownObjectException;
 import model.SNDRC;
 
@@ -68,17 +73,58 @@ public final class StrongInequalityGenerator extends AbstractCutGenerator<SNDRC,
 			throw new RuntimeException("Error, duplicate strong cut is being generated! This cut should already exist in the master problem: "+strongInequality);
 		
 		//Create the inequality in cplex
-		
-		
-		
+		try {
+			IloLinearNumExpr expr=masterData.cplex.linearNumExpr();
+			expr.addTerm(1, masterData.x.get(strongInequality.commodity).get(strongInequality.edgeIndex));
+			
+			//register the columns with this constraints
+			for(Cycle cycle:masterData.getColumns()) {
+				if(cycle.edgeIndexSet.contains(strongInequality.edgeIndex)) {
+					IloNumVar var=masterData.getVar(cycle);
+					expr.addTerm(-dataModel.demandSet.get(strongInequality.commodity).volume, var);
+				}
+			}
+			
+			
+			IloRange strongConstraint=masterData.cplex.addGe(0, expr, "strong cut");
+			masterData.strongInequalities.put(strongInequality, strongConstraint);
+			
+		} catch (IloException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 	}
 	
 	
+	/**
+	 * Add a strong inequality to the master problem
+	 * @param cut AbstractInequality
+	 */
+	@Override
+	public void addCut(AbstractInequality cut) {
+		if(!(cut instanceof StrongInequality))
+			throw new IllegalArgumentException("This AbstractCutGenerator can ONLY add StrongInequalities");
+		StrongInequality strongInequality=(StrongInequality) cut;
+		this.addCut(strongInequality);
+	}
 	
 	
-	
+	/**
+	 * Retuns a list of inequalities that have been generated.
+	 * @return Retuns a list of inequalities that have been generated.
+	 */
+	@Override
+	public List<AbstractInequality> getCuts() {
+		return new ArrayList<>(masterData.strongInequalities.keySet());
+	}
+
+	/**
+	 * Close the generator
+	 */
+	@Override
+	public void close() {} //Nothing to do here
 	
 	
 	
